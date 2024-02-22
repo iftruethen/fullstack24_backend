f8afdad63ee3d0300baf34100609ec9957fd0ab3
iftruedo <jere.ahkiola@gmail.com>
2024-02-22 23:16:48 +0200

    tehtävä 3.8 tehty

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require('express')
 let morgan = require('morgan')
 const app = express()
 app.use(express.json())
-app.use(morgan('tiny'))
+morgan.token('postBody', (req,res) => JSON.stringify(req.body))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :postBody'))
+
 
 let persons = [
     { id: 1, name: 'Arto Hellas', number: '040-123456' },
